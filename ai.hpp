7d6e2be622daf0382e49044e754796058b0aff5b
ThatLukeDev <luke@l2j.co.uk>
2024-05-07 15:56:25 +0100

    mean squared error decreasing

--- ai.hpp
+++ ai.hpp
@@ -5,57 +5,77 @@
 
 #include "matrix.hpp"
 
-namespace ai {
-	template <typename T, unsigned int size>
-	class input {
-	public:
-		matrix<T, 1, size> data = matrix<T, 1, size>();
+template <typename T, unsigned int size>
+class input {
+public:
+	matrix<T, 1, size> data = matrix<T, 1, size>();
 
-		input() { }
-		input(std::vector<T> in) {
-			for (unsigned int i = 0; i < size; i++) {
-				data[0][i] = in[i];
-			}
+	input() { }
+	input(std::vector<T> in) {
+		for (unsigned int i = 0; i < size; i++) {
+			data[0][i] = in[i];
 		}
-	};
-
-	template <typename T, unsigned int size>
-	class bias {
-	public:
-		matrix<T, 1, size> data = matrix<T, 1, size>();
-
-		bias() { }
-		bias(std::vector<T> in) {
-			for (unsigned int i = 0; i < size; i++) {
-				data[0][i] = in[i];
-			}
-		}
-	};
-
-	template <unsigned int size, unsigned int depth>
-	class dense {
-	public:
-		matrix<double, size, depth> data = matrix<double, size, depth>();
-
-		dense() {
-			for (unsigned int x = 0; x < size; x++) {
-				for (unsigned int y = 0; y < depth; y++) {
-					data[x][y] = 1 - 2 * (double(rand()) / RAND_MAX);
-				}
-			}
-		}
-	};
-
-	template <typename T, unsigned int insize, unsigned int depth, unsigned int outsize>
-	void compute(input<T, outsize>* out, input<T, insize> in, dense<insize, depth> l, bias<T, insize> b) {
-		input<T, insize> working = input<T, insize>();
-		working.data = in.data;
-
-		working.data = l.data * working.data;
-		working.data = working.data + b.data;
-
-		out.data = working.data;
 	}
+
+	void norm() {
+		for (unsigned int i = 0; i < size; i++) {
+			if (data[0][i] < 0) {
+				data[0][i] = 0;
+			}
+		}
+	}
+};
+
+template <unsigned int size>
+class bias {
+public:
+	matrix<double, 1, size> data = matrix<double, 1, size>();
+
+	bias() {
+		for (unsigned int y = 0; y < size; y++) {
+			data[0][y] = 1 - 2 * (double(rand()) / RAND_MAX);
+		}
+	}
+
+	void tweak(double lr) {
+		for (unsigned int y = 0; y < size; y++) {
+			data[0][y] += lr * (1 - 2 * (double(rand()) / RAND_MAX));
+		}
+	}
+};
+
+template <unsigned int size, unsigned int depth>
+class dense {
+public:
+	matrix<double, depth, size> data = matrix<double, depth, size>();
+
+	dense() {
+		for (unsigned int x = 0; x < depth; x++) {
+			for (unsigned int y = 0; y < size; y++) {
+				data[x][y] = 1 - 2 * (double(rand()) / RAND_MAX);
+			}
+		}
+	}
+
+	void tweak(double lr) {
+		for (unsigned int x = 0; x < depth; x++) {
+			for (unsigned int y = 0; y < size; y++) {
+				data[x][y] += lr * (1 - 2 * (double(rand()) / RAND_MAX));
+			}
+		}
+	}
+};
+
+template <typename T, unsigned int insize, unsigned int depth, unsigned int outsize>
+void compute(input<T, outsize>* out, input<T, insize> in, dense<insize, depth> l, bias<insize> b) {
+	input<T, insize> working = input<T, insize>();
+	working.data = in.data;
+
+	working.data = l.data * working.data;
+	working.norm();
+	working.data = working.data + b.data;
+
+	out->data = working.data;
 }
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,5 +3,53 @@
 #include "ai.hpp"
 
 int main() {
+	input<double, 2> indata[] = {
+		input<double, 2>(std::vector<double>{0, 0}),
+		input<double, 2>(std::vector<double>{0, 1}),
+		input<double, 2>(std::vector<double>{1, 0}),
+		input<double, 2>(std::vector<double>{1, 1})
+	};
+	double outdata[] = {
+		0,
+		1,
+		1,
+		0
+	};
+	int datasize = 4;
+	int epochs = 500;
+	double lr = 0.01;
+
+	dense<2, 10> denselayer = dense<2,10>();
+	bias<2> biaslayer = bias<2>();
+
+	double lastmse = 2147483647;
+
+	for (int epoch = 0; epoch < epochs; epoch++) {
+		auto newdense = denselayer;
+		auto newbias = biaslayer;
+		newdense.tweak(lr);
+		newbias.tweak(lr);
+
+		double mse = 0;
+
+		for (int i = 0; i < datasize; i++) {
+			input<double, 2> out = input<double, 2>();
+			compute<double, 2, 10, 2>(&out, indata[i], newdense, newbias);
+			double mselocal = outdata[i] - out.data[0][0];
+			mselocal *= mselocal;
+			mse += mselocal;
+		}
+
+		if (mse < lastmse) {
+			denselayer = newdense;
+			biaslayer = newbias;
+			lastmse = mse;
+		}
+
+		if (epoch % (epochs / 10) == 0) {
+			std::cout << "Training... MSE: " << lastmse << std::endl;
+		}
+	}
+
 	return 0;
 }
